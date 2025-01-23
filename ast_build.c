834862c26d5539592196911e41a6d234acf859bf
zerochil <redorangin@gmail.com>
2025-01-23 16:30:27 +0100

    fix: ast subshell siggev and its norminette

--- ast_build.c
+++ ast_build.c
@@ -63,44 +63,40 @@ t_ast_node	*pipeline(t_array *tokens)
 
 t_ast_node	*command(t_array *tokens)
 {
-	t_token		*token;
-	t_ast_node	*subshell_node;
+	t_token	*token;
 
 	token = array_peek(tokens);
 	if (token->type == lexem_get_type("OPEN_PARENTHESIS"))
 	{
 		array_shift(tokens);
-		subshell_node = subshell(tokens);
-		if (subshell_node->children->size == 0)
-			return (syntax_error(ERR_EMPTY_COMMAND), NULL);
-		return (subshell_node);
+		return (subshell(tokens));
 	}
 	return (simple_command(tokens));
 }
 
 t_ast_node	*subshell(t_array *tokens)
 {
-	t_array		*compound_list;
 	t_ast_node	*compound_node;
 	t_token		*token;
 	t_ast_node	*subshell_node;
 
-	compound_list = array_create();
+	subshell_node = create_ast_node(array_create(), AST_SUBSHELL);
 	while (true)
 	{
 		skip_linebreak(tokens);
 		compound_node = compound_command(tokens);
 		if (compound_node == NULL)
 			return (NULL);
-		array_push(compound_list, compound_node);
+		array_push(subshell_node->children, compound_node);
 		skip_linebreak(tokens);
 		token = array_peek(tokens);
 		if (token->type == lexem_get_type("CLOSE_PARENTHESIS"))
-			array_shift(tokens);
-		if (token->type == lexem_get_type("CLOSE_PARENTHESIS"))
 			break ;
 	}
-	subshell_node = create_ast_node(compound_list, AST_SUBSHELL);
+	if (subshell_node->children->size == 0)
+		return (syntax_error(ERR_EMPTY_COMMAND), NULL);
+	if (token->type == lexem_get_type("CLOSE_PARENTHESIS"))
+		array_shift(tokens);
 	subshell_node->redirect_list = consume_until(tokens, lexem_is_redirection,
 			NULL, is_filename_missing);
 	return (subshell_node);
